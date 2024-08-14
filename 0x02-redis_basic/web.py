a3f7a3f8865eb3220c0b588e31c620b70e6927b0
root <root@DESKTOP-2I6K092>
2024-08-14 23:14:21 +0100

    task 5__ redis

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -63,9 +63,3 @@ def get_page(url: str) -> str:
     """
     response = requests.get(url)
     return response.text
-
-
-if __name__ == "__main__":
-    url = "http://slowwly.robertomurray.co.uk"
-    print(get_page(url))
-    print(f"URL accessed {cache.get_count(url)} times")
