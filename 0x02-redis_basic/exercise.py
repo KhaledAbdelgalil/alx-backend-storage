0b486ed97499b37527fa86abf7c6024548006e5c
root <root@DESKTOP-2I6K092>
2024-08-14 20:44:51 +0100

    task 3 redis

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -18,6 +18,7 @@ def count_calls(method: Callable) -> Callable:
         return method(self, *args, **kwds)
     return wrapper
 
+
 def call_history(method: Callable) -> Callable:
     """ store the history of inputs and outputs for a particular function """
     key = method.__qualname__
