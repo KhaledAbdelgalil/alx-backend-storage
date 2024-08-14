e09810e76c8fca53631bba7d0433893f10199247
root <root@DESKTOP-2I6K092>
2024-08-14 23:07:36 +0100

    task 5__ redis

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,26 +1,33 @@
 #!/usr/bin/env python3
-""" Implementing an expiring web cache and tracker
-    obtain the HTML content of a particular URL and return it """
+""" Implementing an expiring web cache and tracker with a decorator """
 import redis
 import requests
-from typing import Optional
+from typing import Callable, Optional
 
-r = redis.Redis()
+r = redis.Redis(host='localhost', port=6379, db=0, decode_responses=True)
 
 
+def cache_with_expiry(expiry: int = 10) -> Callable:
+    """ Decorator to cache the result of a function with an expiry time """
+    def decorator(func: Callable) -> Callable:
+        def wrapper(url: str) -> str:
+            cached_content: Optional[str] = r.get(f"cached:{url}")
+            if cached_content:
+                return cached_content
+
+            content = func(url)
+
+            r.incr(f"count:{url}")
+
+            r.setex(f"cached:{url}", expiry, content)
+
+            return content
+        return wrapper
+    return decorator
+
+
+@cache_with_expiry(expiry=10)
 def get_page(url: str) -> str:
-    """ Track how many times a particular URL was accessed in the key
-        "count:{url}" and cache the result with an expire time of 10 seconds"""
-
-    cached_content: Optional[bytes] = r.get(f"cached:{url}")
-    if cached_content:
-        return cached_content.decode('utf-8')
-
+    """ Obtain the HTML content of a particular URL and return it """
     response = requests.get(url)
-    content = response.text
-
-    r.incr(f"count:{url}")
-
-    r.setex(f"cached:{url}", 10, content)
-
-    return content
+    return response.text
